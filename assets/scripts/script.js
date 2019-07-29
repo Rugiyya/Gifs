baf19a9bb81b15a3c99454148ff8d0b0915e6b89
Rugiyya Mammadov <rugiyamammadova@gmail.com>
2019-07-29 18:58:14 -0400

    fixed js

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -7,9 +7,16 @@ function createCard(response) {
 
 
     var catImage = $("<img>");
-    catImage.attr("src", response.Images);
-    catImage.attr("alt", response.Title + " Images");
-    $("#cat-section").append(catImage);
+    catImage.attr("src", response.data.original_still);
+
+    var catImage = $("<figure>");
+    catImage.append(giphyImage);
+    article.append(giphyImage);
+
+    var cardBody = $("<div>");
+    cardBody.addClass("card-body");
+
+    $("#cat-section").append(article);
 }
 
 function getGif(cat) {
@@ -21,13 +28,23 @@ function getGif(cat) {
         method: "GET"
     }).done(function (response) {
         console.log(response);
-        createCard(response);
-        giphyList.push(response.Title);
+        if (response.Response === "False") {
+            alert(response.Error);
+        } else if (giphy.indexOf(response.data.original_still) >= 0) {
+            alert("Gif already in List!")
+        } else {
+            createCard(response);
+            gif.push(response.Title);
+        }
     });
+
 }
+
+
+
 for (var i = 0; i < giphyList.length; i++) {
 
-    var movie = giphyList[i];
+    var giphy = giphyList[i];
     getGif(cat);
     console.log(giphyList);
 
